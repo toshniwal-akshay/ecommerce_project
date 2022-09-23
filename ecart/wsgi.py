96372e381f40d9d83a6d635aa49f107636eba931
toshniwal.akshay <atoshniwal@deqode.com>
2022-09-23 22:56:48 +0530

    Updated Settings File for StaticFile

--- ecart/wsgi.py
+++ ecart/wsgi.py
@@ -18,5 +18,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecart.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
